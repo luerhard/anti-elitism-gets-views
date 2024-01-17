4a07825f2992e6e8a42fccafe79feeb558f1d05e
Lukas Erhard <luerhard@googlemail.com>
2024-01-17 12:23:33 +0100

    4 video downloader (#12)
    * add yt-dlp to dependencies
    
    * first simple implementation of YTDownload.
    
    Can actually download a video.
    
    * add a test
    
    * add download params to ytcrawler
    
    * add extract_info method
    
    * add template for db inserts
    
    * fix formatting
    
    * add info.json to testdata
    
    * wip
    
    * YTCrawler can now add a video to a database
    
    * add torch dep
    
    * basic download and insert single video
    
    * fix back_populates warning
    
    * wip
    
    * fix test
    
    * should be a working yt-channel crawler now

---
+++ src/data/models.py
@@ -0,0 +1,65 @@
+# ruff: noqa: D101
+"""Sqlalchemy models for data collection process."""
+
+from sqlalchemy import BigInteger
+from sqlalchemy import Boolean
+from sqlalchemy import Column
+from sqlalchemy import Date
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Channel(Base):
+    __tablename__ = "channels"
+
+    id = Column(String, primary_key=True)
+    channel = Column(String, index=True)
+    uploader_id = Column(String, index=True)
+    title = Column(String)
+    description = Column(String)
+    channel_follower_count = Column(BigInteger)
+    channel_url = Column(String)
+    playlist_count = Column(Integer)
+
+    videos = relationship("Video", uselist=True, back_populates="channel")
+
+
+class Video(Base):
+    __tablename__ = "videos"
+
+    id = Column(String, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    channel_id = Column(String, ForeignKey("channels.id"), index=True)
+    datetime_upload = Column(Date)
+    duration = Column(Integer)
+    view_count = Column(BigInteger)
+    like_count = Column(BigInteger)
+    comment_count = Column(BigInteger)
+    was_live = Column(Boolean)
+    relative_file_path = Column(String)
+
+    comments = relationship("Comment", uselist=True, back_populates="video")
+    channel = relationship("Channel", uselist=False, back_populates="videos")
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id = Column(String, primary_key=True)
+    video_id = Column(String, ForeignKey("videos.id"), nullable=False)
+    text = Column(String)
+    datetime_upload = Column(DateTime)
+    parent = Column(String)
+    like_count = Column(Integer)
+    author = Column(String)
+    author_is_uploader = Column(Boolean)
+    is_favorited = Column(Boolean)
+
+    video = relationship("Video", uselist=False, back_populates="comments")
---
+++ tests/crawl/test_yt_crawler.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import pytest
+from sqlalchemy import create_engine
+
+from src.crawl.yt_crawler import YTChannelCrawler
+from src.crawl.yt_crawler import YTDownload
+
+@pytest.mark.online()
+def test_download(tmp_path: Path):
+    ydl = YTDownload(output=tmp_path)
+    # yt-dlp test video
+    url = "https://www.youtube.com/watch?v=BaW_jenozKc"
+    ydl.download(url)
+
+    assert len(list(tmp_path.iterdir())) == 1
+
+
+@pytest.mark.online()
+def test_extract_video_info(tmp_path: Path):
+    ydl = YTDownload(output=tmp_path)
+    # yt-dlp test video
+    url = "https://www.youtube.com/watch?v=BaW_jenozKc"
+    info = ydl.extract_video_info(url)
+
+    assert info["title"] == "youtube-dl test video \"'/\\ä↭𝕐"
+    assert info["id"] == "BaW_jenozKc"
+
+
+class TestDBInsert:
+    @classmethod
+    def setup_class(cls):
+        cls.engine = create_engine("sqlite:////home/lukas/Desktop/testing/test.sqlite")
+        # cls.engine = create_engine("sqlite:///:memory:")
+
+    @pytest.mark.online()
+    def test_download_and_insert(self, tmp_path: Path):
+        ytc = YTChannelCrawler(engine=self.engine, output=tmp_path)
+        # yt-dlp test video
+        url = "https://www.youtube.com/watch?v=BaW_jenozKc"
+
+        ytc._download_video(url)
+
+    @pytest.mark.online()
+    def test_download_channel(self):
+        ytc = YTChannelCrawler(
+            engine=self.engine,
+            output="/home/lukas/Desktop/testing/channel_videos",
+        )
+        # yt-dlp test video
+        channel_url = "https://www.youtube.com/@sonorityofficial9831"
+
+        ytc.download_channel(channel_url)

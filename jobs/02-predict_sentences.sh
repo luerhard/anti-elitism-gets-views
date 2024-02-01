e940772270a37c7e23eb8fdad02d34e5b134c507
Lukas Erhard <luerhard@googlemail.com>
2024-02-01 18:09:35 +0100

    add job to predict sentences

---
+++ jobs/02-predict_sentences.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=transcribe
+#SBATCH --partition=single
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+#SBATCH --mem=10gb
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=lukas.erhard@sowi.uni-stuttgart.de
+
+module load devel/cuda/12.1
+
+cd /home/st/st_st/st_ac138201/ws_ytpop/ytpop
+
+poetry install
+poetry run python pipeline/03-predict_sentences.py
+

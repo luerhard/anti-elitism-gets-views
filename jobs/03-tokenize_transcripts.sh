c3e85cab447f3538d8f26038a4ab640328daa6ec
Lukas Erhard <luerhard@googlemail.com>
2024-02-06 16:34:40 +0100

    test new pipeline

--- jobs/03-tokenize_transcripts.sh
+++ jobs/03-tokenize_transcripts.sh
@@ -3,16 +3,12 @@
 #SBATCH --partition=single
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=10
-#SBATCH --gres=gpu:1
-#SBATCH --time=24:00:00
+#SBATCH --time=08:00:00
 #SBATCH --mem=10gb
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=lukas.erhard@sowi.uni-stuttgart.de
 
-module load devel/cuda/12.1
-
 cd /home/st/st_st/st_ac138201/ws_ytpop/ytpop
 
 poetry install
-poetry run python pipeline/03-predict_sentences.py
-
+poetry run python pipeline/03-tokenize_transcripts.py

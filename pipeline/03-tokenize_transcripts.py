0b951823d927ce08b5153d6a671b250fabb11612
Lukas Erhard <luerhard@googlemail.com>
2024-02-06 16:34:40 +0100

    test new pipeline

--- pipeline/03-tokenize_transcripts.py
+++ pipeline/03-tokenize_transcripts.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy.orm import close_all_sessions
+
 import src
 from src.data.models import Base
 from src.data.models import Sentence
 from src.data.models import Transcript
+from src.data.transcript_processor import TranscriptCleaner
 from src.logging import logger as log
-from src.data.transcript_processor import TranscriptProcessor
-
 
 ENGINE = create_engine(src.PS_ENGINE)
 
@@ -24,34 +23,20 @@ def main():
     Base.metadata.create_all(ENGINE, tables=[Sentence.__table__])
     log.info("created all tables.")
 
-    processor = TranscriptProcessor()
+    cleaner = TranscriptCleaner()
     log.info("Processor loaded.")
-
-    elite_thresh = float(src.config["THRESHOLD"]["elite"])
-    pplcentr_thresh = float(src.config["THRESHOLD"]["pplcentr"])
-    left_thresh = float(src.config["THRESHOLD"]["left"])
-    right_thresh = float(src.config["THRESHOLD"]["right"])
 
     session = Session(bind=ENGINE, expire_on_commit=False)
     for transcript_no, transcript in enumerate(iter_transcripts(ENGINE)):
-        log.debug("Processing transcript: %s", transcript.id)
+        log.debug("Processing transcript (%d): %s", transcript_no, transcript.id)
         text = transcript.text
-        sentences = processor.tokenize(text)
+        sentences = cleaner.tokenize(text)
         for sentence_no, sentence in enumerate(sentences, 1):
             if not sentence:
                 continue
-            predictions = processor.predict_populism(sentence)
-            elite = 1 if predictions[0] > elite_thresh else 0
-            pplcentr = 1 if predictions[1] > pplcentr_thresh else 0
-            left = 1 if predictions[2] > left_thresh else 0
-            right = 1 if predictions[3] > right_thresh else 0
             row = Sentence(
                 video_id=transcript.id,
                 sentence_no=sentence_no,
-                elite=elite,
-                pplcentr=pplcentr,
-                left=left,
-                right=right,
                 tokens=sentence,
             )
             session.add(row)
---
+++ pipeline/04-run_popbert.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+import src
+from src.data.models import Sentence
+from src.data.transcript_processor import PopBERTPredictor
+from src.logging import logger as log
+from src.utils.iterate import chunks
+
+ENGINE = create_engine(src.PS_ENGINE)
+CHUNKSIZE = 2
+
+
+def iter_sentences(session):
+    query = session.query(Sentence).yield_per(500)
+    yield from chunks(query, chunksize=CHUNKSIZE)
+
+
+def main():
+    popbert = PopBERTPredictor()
+    log.info("PopBERT loaded.")
+
+    session = Session(bind=ENGINE, expire_on_commit=False)
+    for chunk_no, sentences in enumerate(iter_sentences(session), 1):
+        log.debug("Processing chunk: %d", chunk_no)
+        tokens = [sent.tokens for sent in sentences]
+        predictions = popbert.predict(tokens, chunksize=CHUNKSIZE)
+        for sentence, pred in zip(sentences, predictions, strict=False):
+            sentence.elite = pred[0]
+            sentence.pplcentr = pred[1]
+            sentence.left = pred[2]
+            sentence.right = pred[3]
+
+        session.add_all(sentences)
+        if not chunk_no % 100:
+            session.commit()
+            break
+    # session.commit()
+    ENGINE.dispose()
+
+
+if __name__ == "__main__":
+    main()

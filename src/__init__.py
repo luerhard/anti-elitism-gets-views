77c9d8bab0dcbe65d1be71e1b4b1cd52ce347c21
Lukas Erhard <luerhard@googlemail.com>
2024-02-09 14:21:05 +0100

    better descriptives

--- src/__init__.py
+++ src/__init__.py
@@ -22,8 +22,10 @@ colormap = {
     "CDU": "#000000",
     "CSU": "#000000",
     "Grüne": "#1AA037",
+    "Greens": "#1AA037",
     "DIE LINKE": "#8B008B",  # SPD complementary for visual disambiguation
     "Linke": "#8B008B",  # SPD complementary for visual disambiguation
+    "Left": "#8B008B",  # SPD complementary for visual disambiguation
     "FDP": "#FFEF00",
     "AfD TV": "#0489DB",
     "AfD-Fraktion Bundestag": "#0489DB",
@@ -32,3 +34,9 @@ colormap = {
 }
 
 r_colormap = pd.DataFrame(colormap.items(), columns=["channel", "color"])
+
+party_names = {
+    "BÜNDNIS 90/DIE GRÜNEN": "Greens",
+    "AfD-Fraktion Bundestag": "AfD BT",
+    "DIE LINKE": "Left",
+}

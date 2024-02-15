d3d667ba6a315d02d1f869a9429f79f86410b185
Lukas Erhard <luerhard@googlemail.com>
2024-02-15 17:46:36 +0100

    nb regression in weird notebook

--- src/data/load.py
+++ src/data/load.py
@@ -16,6 +16,7 @@ def video_dataset():
     engine = create_engine(src.PS_ENGINE)
     sentence_query = (
         Query(Sentence)
+        .filter(Sentence.is_valid == True)
         .group_by(Sentence.video_id)
         .with_entities(
             Sentence.video_id,
@@ -28,7 +29,10 @@ def video_dataset():
     query = (
         Query(Channel)
         .join(Video)
-        .filter(Video.format == "videos")
+        .filter(
+            Video.format == "videos",
+            Video.is_valid == True,
+        )
         .group_by(Channel.channel, Video.id)
         .join(sentence_query, Video.id == sentence_query.c.video_id)
         .with_entities(
@@ -46,7 +50,7 @@ def video_dataset():
     with engine.connect() as conn:
         df = pd.read_sql(query.statement, conn)
 
-    df.channel = df.channel.replace({"BÜNDNIS 90/DIE GRÜNEN": "Grüne"})
+    df.channel = df.channel.replace(src.party_names)
     df.channel = df.channel.astype("category")
     df.datetime_upload = pd.to_datetime(df.datetime_upload)
     df["populism"] = df.elite * df.pplcentr

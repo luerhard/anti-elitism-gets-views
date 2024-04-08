a89f395b1a02afddcdaf338790a2da765bfc0228
Lukas Erhard <lukas.erhard@sowi.uni-stuttgart.de>
2024-04-08 21:06:01 +0200

    add popbert stage

--- pipeline/04-run_popbert.py
+++ pipeline/04-run_popbert.py
@@ -1,42 +1,67 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+import ibis
 
 import src
-from src.data.models import Sentence
-from src.data.predictors import PopBERTPredictor
 from src.logging import logger as log
-from src.utils.iterate import chunks
+from src.processing.predictors import PopBERTPredictor
 
-ENGINE = create_engine(src.PS_ENGINE)
 CHUNKSIZE = 64
-
-
-def iter_sentences(session):
-    query = session.query(Sentence).execution_options(stream_results=True, max_row_buffer=5000)
-    yield from chunks(query, chunksize=CHUNKSIZE)
+DB_PATH = src.PATH / "tmp/popbert.duckdb"
+SENTENCE_PATH = src.PATH / "data/interim/sentences.parquet.gzip"
+OUT_FILE = src.PATH / "data/interim/popbert.parquet.gzip"
 
 
 def main():
+    con = ibis.connect(DB_PATH)
+    if "sentences" not in con.list_tables():
+        table_sentences = con.read_parquet(SENTENCE_PATH)
+    else:
+        table_sentences = con.table("sentences")
+
+    if "popbert" not in con.list_tables():
+        schema = ibis.schema(
+            {
+                "video_id": "string",
+                "sentence_no": "int32",
+                "elite": "float64",
+                "pplcentr": "float64",
+                "left": "float64",
+                "right": "float64",
+            },
+        )
+        table_popbert = con.create_table("popbert", schema=schema)
+    else:
+        table_popbert = con.table("popbert")
+    log.info("DB loaded.")
+
     popbert = PopBERTPredictor()
     log.info("PopBERT loaded.")
 
-    session = Session(bind=ENGINE, expire_on_commit=False)
-    for chunk_no, sentences in enumerate(iter_sentences(session), 1):
-        log.debug("Processing chunk: %d", chunk_no)
-        tokens = [sent.tokens for sent in sentences]
+    sentences_df = table_sentences.filter(
+        table_sentences.video_id.notin(table_popbert.video_id),
+    ).to_pandas()
+
+    i = 0
+    for video_id, video_df in sentences_df.groupby("video_id"):
+        i += len(video_df)
+        log.info("Processing (%d/%d): %s", i, len(sentences_df), video_id)
+        cache = []
+        tokens = video_df.tokens.to_list()
+        sentence_numbers = video_df.sentence_no.to_list()
         predictions = popbert.predict(tokens, chunksize=CHUNKSIZE)
-        for sentence, pred in zip(sentences, predictions, strict=False):
-            sentence.elite = pred[0]
-            sentence.pplcentr = pred[1]
-            sentence.left = pred[2]
-            sentence.right = pred[3]
+        for sentence_no, prediction in zip(sentence_numbers, predictions, strict=True):
+            row = {
+                "video_id": video_id,
+                "sentence_no": sentence_no,
+                "elite": prediction[0],
+                "pplcentr": prediction[1],
+                "left": prediction[2],
+                "right": prediction[3],
+            }
+            cache.append(row)
+        con.insert("popbert", cache)
 
-        session.add_all(sentences)
-        if not chunk_no % 100:
-            session.commit()
-    session.commit()
-    ENGINE.dispose()
-
+    table_popbert.to_parquet(OUT_FILE, compression="gzip")
+    DB_PATH.unlink(missing_ok=False)
 
 if __name__ == "__main__":
     main()

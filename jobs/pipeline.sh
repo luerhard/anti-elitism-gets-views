33bb82cd47098a29df463c61e697e7a94cea2819
Lukas Erhard <lukas.erhard@sowi.uni-stuttgart.de>
2024-04-08 17:12:17 +0200

    add general pipeline job

---
+++ jobs/pipeline.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+#SBATCH --job-name=dvc_pipeline
+#SBATCH --partition=single
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --gres=gpu:1
+#SBATCH --time=48:00:00
+#SBATCH --mem=30gb
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=lukas.erhard@sowi.uni-stuttgart.de
+
+module load devel/cuda/12.1
+
+cd /home/st/st_st/st_ac138201/ws_ytpop/ytpop
+
+poetry install
+dvc repro

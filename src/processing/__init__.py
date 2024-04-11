3c821ef0b0fec05002d7a41f16a141f21fd8801f
Lukas Erhard <luerhard@googlemail.com>
2024-04-11 10:45:40 +0200

    prepare for roberta run

--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -1,14 +1,1 @@
-"""Handle all data import and export."""
-
-import sqlalchemy
-
-import src
-
-def create_engine():
-    """Create a sqlite DB engine."""
-    db_path = f"sqlite:///{src.PATH}/data/db/sqlite.db"
-    engine = sqlalchemy.create_engine(db_path)
-    return engine
-
-
-__all__ = ["create_engine"]
+"""Handle all data processing classes and functions"""

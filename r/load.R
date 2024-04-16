ff2c09ca19b75549b575669c39a7cae8adf134ab
Lukas Erhard <luerhard@googlemail.com>
2024-04-16 19:41:28 +0200

    Mv to typst (#26)
    * add simple typst template
    
    * finish nb_regression stage

--- r/load.R
+++ r/load.R
@@ -5,10 +5,26 @@ box::use(
 
 reticulate::use_python(here(".venv/bin/python"))
 
-video_dataset <- function() {
-  # reticulate::import("src.data.load.video_dataset")
-  data_load <- reticulate::import("src.data.load")
-  df <- data_load$video_dataset()
+channels <- function() {
+  data_load <- reticulate::import("src.load")
+  df <- data_load$channels()
+  return(df)
+}
 
+videos <- function() {
+  data_load <- reticulate::import("src.load")
+  df <- data_load$videos()
+  return(df)
+}
+
+sentences <- function() {
+  data_load <- reticulate::import("src.load")
+  df <- data_load$sentences()
+  return(df)
+}
+
+popbert <- function() {
+  data_load <- reticulate::import("src.load")
+  df <- data_load$popbert()
   return(df)
 }

454362d2efeaa01a43f56f8b7b9f38c1067cc6e6
Lukas Erhard <luerhard@googlemail.com>
2024-04-16 14:40:44 +0200

    Invalid data (#25)
    * add basic load template
    
    * restructure predictors. return actual probs in popbert, not binarized result
    
    * start data loader
    
    * run popbert
    
    * add device to manifesto predictor
    
    * run manifesto roberta
    
    * renaming columns and add category types
    
    * add basic_descriptives stage
    
    * add papermill
    
    * run basic_descriptives
    
    * precommit update
    
    * add nbstripout

--- src/processing/manifesto_predictor.py
+++ src/processing/manifesto_predictor.py
@@ -1,117 +1,16 @@
-from abc import ABCMeta
-from abc import abstractmethod
-from abc import abstractproperty
 
 import numpy as np
 import torch
 from transformers import AutoModelForSequenceClassification
 from transformers import AutoTokenizer
 
-import src
 from src.logging import logger as log
 from src.utils.iterate import chunks
 from src.utils.iterate import flatten_list
 
-class TransformerPredictor(metaclass=ABCMeta):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    @abstractmethod
-    def __init__(self) -> None: ...
-
-    @abstractproperty
-    @property
-    def model(self): ...
-
-    @abstractproperty
-    @property
-    def tokenizer(self): ...
-
-    @abstractmethod
-    def tokenize(self): ...
-
-    @abstractmethod
-    def _get_probas(self, out): ...
-
-    @abstractmethod
-    def predict(self): ...
-
-
-class PopBERTPredictor(TransformerPredictor):
+class ManifestoPredictor:
     def __init__(self) -> None:
-        self._tokenizer = None
-        self._model = None
-        self._max_length = None
-        self.elite_thresh = float(src.config["THRESHOLD"]["elite"])
-        self.pplcentr_thresh = float(src.config["THRESHOLD"]["pplcentr"])
-        self.left_thresh = float(src.config["THRESHOLD"]["left"])
-        self.right_thresh = float(src.config["THRESHOLD"]["right"])
-
-        self.thresholds = (
-            self.elite_thresh,
-            self.pplcentr_thresh,
-            self.left_thresh,
-            self.right_thresh,
-        )
-
-    @property
-    def tokenizer(self):
-        if not self._tokenizer:
-            self._tokenizer = AutoTokenizer.from_pretrained("luerhard/PopBERT")
-        return self._tokenizer
-
-    @property
-    def model(self):
-        if not self._model:
-            self._model = AutoModelForSequenceClassification.from_pretrained(
-                "luerhard/PopBERT",
-            ).to(
-                self.device,
-            )
-        return self._model
-
-    def tokenize(self, batch):
-        encodings = self.tokenizer(
-            batch,
-            is_split_into_words=True,
-            truncation=True,
-            padding=True,
-            return_tensors="pt",
-            max_length=512,
-        )
-        return encodings
-
-    def _get_probas(self, out):
-        probs = torch.nn.functional.sigmoid(out.logits)
-        probs = probs.detach().cpu().numpy()
-        labels = np.where(probs > self.thresholds, 1, 0)
-        return labels
-
-    def predict(self, tokens: list[str] | list[list[str]], chunksize=32):
-        """Predict populism dimensions of an already tokenized sentence."""
-        # ensure correct tokens-batch format
-        if len(tokens) < 1:
-            # if tokens is empty, raise Error
-            tokens = [[]]
-        elif isinstance(tokens, list) and isinstance(tokens[0], str):
-            # if tokens is a single sentence wrap in in batch
-            tokens = [tokens]
-
-        results = []
-        for batch in chunks(tokens, chunksize=chunksize):
-            encodings = self.tokenize(batch)
-            encodings = encodings.to(self.device)
-
-            with torch.inference_mode():
-                out = self.model(**encodings)
-
-            probas = self._get_probas(out)
-            results.extend(probas)
-
-        return results
-
-
-class ManifestorPredictor(TransformerPredictor):
-    def __init__(self) -> None:
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self._tokenizer = None
         self._model = None
         self._max_length = None

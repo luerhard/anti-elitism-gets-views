5cc33df6b097a186380c48c6057287222d5da549
Lukas Erhard <luerhard@googlemail.com>
2025-02-01 01:50:45 +0100

    fix tests

--- tests/crawl/test_yt_crawler.py
+++ tests/crawl/test_yt_crawler.py
@@ -1,44 +1,26 @@
 from pathlib import Path
 
 import pytest
-from sqlalchemy import create_engine
 
-from src.crawl.yt_crawler import YTChannelCrawler
 from src.crawl.yt_crawler import YTDownload
 
 
-@pytest.mark.online()
+@pytest.mark.online
 def test_download(tmp_path: Path):
     ydl = YTDownload(output=tmp_path)
     # yt-dlp test video
-    url = "https://www.youtube.com/watch?v=BaW_jenozKc"
+    url = "https://www.youtube.com/watch?v=DTi8wZ1a1TA"
     ydl.download(url)
 
     assert len(list(tmp_path.iterdir())) == 1
 
 
-@pytest.mark.online()
+@pytest.mark.online
 def test_extract_video_info(tmp_path: Path):
     ydl = YTDownload(output=tmp_path)
     # yt-dlp test video
-    url = "https://www.youtube.com/watch?v=BaW_jenozKc"
+    url = "https://www.youtube.com/watch?v=DTi8wZ1a1TA"
     info = ydl.extract_video_info(url)
 
-    assert info["title"] == "youtube-dl test video \"'/\\ä↭𝕐"
-    assert info["id"] == "BaW_jenozKc"
-
-
-class TestDBInsert:
-    @classmethod
-    def setup_class(cls):
-        # cls.engine = create_engine("sqlite:////home/lukas/Desktop/testing/test.sqlite")
-        cls.engine = create_engine("sqlite:///:memory:")
-
-    @pytest.mark.online()
-    def test_download_channel(self, tmp_path):
-        ytc = YTChannelCrawler(
-            engine=self.engine,
-            channel_url="https://www.youtube.com/@sonorityofficial9831",
-            output=tmp_path,
-        )
-        ytc.download_channel_videos()
+    assert info["title"] == "youtube-dl test video [BaW_jenozKc]"
+    assert info["id"] == "DTi8wZ1a1TA"

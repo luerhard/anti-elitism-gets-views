7f9eb4ba2114c8db047744f3623a4bbc30203285
Lukas Erhard <luerhard@googlemail.com>
2025-02-13 12:11:11 +0100

    better pipeline. remove duckdb dependency in graph, use persist

--- pipeline/02-run_asr.py
+++ pipeline/02-run_asr.py
@@ -7,18 +7,21 @@ from src.logging import logger as log
 from src.processing.whisper_pipeline import WhisperPipeline
 
 BASE_VIDEO_PATH = src.PATH / "data/raw/yt/"
-DB_PATH = src.PATH / "data/interim/transcripts/v3_large_turbo.duckdb"
+DB_PATH = src.PATH / "tmp/v3_large_turbo.duckdb"
 OUT_FILE = src.PATH / "data/interim/transcripts/v3_large_turbo.parquet"
 
 
 def main():
-    con = ibis.connect(f"duckdb://{DB_PATH}")
+    con = ibis.connect(f"duckdb://{DB_PATH}", memory_limit="5GB")
     if "videos" not in con.list_tables():
         videos = con.read_parquet(src.PATH / "data/yt_metadata/videos.parquet")
     else:
         videos = con.table("videos")
 
-    if "transcripts" not in con.list_tables():
+    if "transcripts" not in con.list_tables() and OUT_FILE.is_file():
+        expr = con.read_parquet(OUT_FILE)
+        transcripts = con.create_table("transcripts", expr)
+    elif "transcripts" not in con.list_tables():
         schema = ibis.schema({"video_id": "string", "text": "string"})
         transcripts = con.create_table(name="transcripts", schema=schema)
     else:
@@ -27,23 +30,31 @@ def main():
     pipeline = WhisperPipeline(model_type="large-v3-turbo")
     log.warning("Pipeline loaded.")
 
-    video_df = videos.filter(
+    data = videos.filter(
         [
             videos.video_was_live == False,
             videos.video_datetime_upload >= "2017-12-06",
             videos.video_datetime_upload <= "2025-02-01",
-            videos.video_id.notin(transcripts.video_id),
         ],
-    ).to_pandas()
-    log.warning("%d videos left to transcribe", len(video_df))
-    for i, video in enumerate(video_df.itertuples(), 1):
-        log.info("Processing (%d/%d): %s", i, len(video_df), video.video_id)
+    ).anti_join(transcripts, ["video_id"])
+    n_total = data.count().execute()
+    done = 0
+    log.info("Start Processing")
+    while True:
+        video_df = data.limit(1).to_pandas()
+        if video_df.empty:
+            break
+        video = video_df.iloc[0]
+
+        done += 1
+        log.info("Processing (%d/%d): %s", done, n_total, video.video_id)
         file_path = BASE_VIDEO_PATH / video.channel / "videos" / f"{video.video_id}.m4a"
         text = pipeline.transcribe(file_path)
         transcript = {"video_id": video.video_id, "text": text}
         con.insert("transcripts", [transcript])
 
     transcripts.to_parquet(OUT_FILE, compression="gzip")
+    DB_PATH.unlink(missing_ok=False)
 
 
 if __name__ == "__main__":

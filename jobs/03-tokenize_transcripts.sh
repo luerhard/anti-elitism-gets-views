a943b2a208eb51b8854584f316da1f9784cd9be7
Lukas Erhard <luerhard@googlemail.com>
2025-02-26 15:05:52 +0100

    update jobs

--- jobs/03-tokenize_transcripts.sh
+++ jobs/03-tokenize_transcripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=transcribe
-#SBATCH --partition=single
+#SBATCH --partition=cpu-single
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=10
 #SBATCH --time=03:00:00
@@ -8,7 +8,6 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=lukas.erhard@sowi.uni-stuttgart.de
 
-cd /home/st/st_st/st_ac138201/ws_ytpop/ytpop
+cd /home/st/st_st/st_ac138201/workspaces/gpfs/st_ac138201-ytpop/ytpop
 
-poetry install
-poetry run python pipeline/03-tokenize_transcripts.py
+~/nix-portable nix develop --impure --command python pipeline/03-tokenize_transcripts.py

8462580778f40e0baa728db708bd9be91aae9e2b
Lukas Erhard <luerhard@googlemail.com>
2025-04-29 16:59:17 +0200

    redownload up until csumedia

--- scripts/crawler/01-crawl_channels.py
+++ scripts/crawler/01-crawl_channels.py
@@ -12,12 +12,12 @@ STORAGE_PATH = src.PATH / "data/raw/yt"
 STORAGE_PATH.mkdir(parents=True, exist_ok=True)
 
 YTCHANNELS = [
-    #"https://www.youtube.com/@AfDFraktionimBundestag",
-    #"https://www.youtube.com/@AfDTV",
-    "https://www.youtube.com/@DieGruenen",
-    "https://www.youtube.com/@gruenebundestag",
-    "https://www.youtube.com/@spdde",
-    "https://www.youtube.com/@spdbt",
+    # "https://www.youtube.com/@AfDFraktionimBundestag",
+    # "https://www.youtube.com/@AfDTV",
+    # "https://www.youtube.com/@DieGruenen",
+    # "https://www.youtube.com/@gruenebundestag",
+    # "https://www.youtube.com/@spdde",
+    # "https://www.youtube.com/@spdbt",
     "https://www.youtube.com/@csumedia",
     "https://www.youtube.com/@csuimbundestag9622",
     "https://www.youtube.com/@cdutv",
@@ -33,7 +33,11 @@ YTCHANNELS = [
 def main():
     for channel_url in YTCHANNELS:
         log.info("Starting Channel: %s", channel_url)
-        ycc = YTChannelCrawler(channel_url=channel_url, output=STORAGE_PATH, start_date="2017-12-06")
+        ycc = YTChannelCrawler(
+            channel_url=channel_url,
+            output=STORAGE_PATH,
+            start_date="2017-12-06",
+        )
         ycc.download_channel()
 
 

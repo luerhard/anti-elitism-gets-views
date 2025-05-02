0b2d06c175acaaec580850f19a9884b8c9b9788a
Lukas Erhard <luerhard@googlemail.com>
2025-05-02 13:01:01 +0200

    re-enable all channels

--- scripts/crawler/01-crawl_channels.py
+++ scripts/crawler/01-crawl_channels.py
@@ -12,18 +12,18 @@ STORAGE_PATH = src.PATH / "data/raw/yt"
 STORAGE_PATH.mkdir(parents=True, exist_ok=True)
 
 YTCHANNELS = [
-    # "https://www.youtube.com/@AfDFraktionimBundestag",
-    # "https://www.youtube.com/@AfDTV",
-    # "https://www.youtube.com/@DieGruenen",
-    # "https://www.youtube.com/@gruenebundestag",
-    # "https://www.youtube.com/@spdde",
-    # "https://www.youtube.com/@spdbt",
-    # "https://www.youtube.com/@csumedia",
-    # "https://www.youtube.com/@csuimbundestag9622",
-    # "https://www.youtube.com/@cdutv",
-    # "https://www.youtube.com/@cducsu",
-    # "https://www.youtube.com/@DIELINKE",
-    # "https://www.youtube.com/@linksfraktion",  # bis Ende 2023
+    "https://www.youtube.com/@AfDFraktionimBundestag",
+    "https://www.youtube.com/@AfDTV",
+    "https://www.youtube.com/@DieGruenen",
+    "https://www.youtube.com/@gruenebundestag",
+    "https://www.youtube.com/@spdde",
+    "https://www.youtube.com/@spdbt",
+    "https://www.youtube.com/@csumedia",
+    "https://www.youtube.com/@csuimbundestag9622",
+    "https://www.youtube.com/@cdutv",
+    "https://www.youtube.com/@cducsu",
+    "https://www.youtube.com/@DIELINKE",
+    "https://www.youtube.com/@linksfraktion",  # bis Ende 2023
     "https://www.youtube.com/@dielinkebt",  # ab 2023
     "https://www.youtube.com/@FDP",
     "https://www.youtube.com/@fdpbt",

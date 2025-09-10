36ae872830af3f8d4d960aa57d133d966a93ee75
Lukas Erhard <luerhard@googlemail.com>
2025-09-10 07:43:00 +0000

    apply R formatter

--- pipeline/06-run_regressions.R
+++ pipeline/06-run_regressions.R
@@ -45,13 +45,38 @@ parties <- df |>
 
 
 model_types <- list(
-  views_elite = formula(video_views ~ channel + log_n_sents + released_year + is_short + elite),
-  views_pplcentr = formula(video_views ~ channel + log_n_sents + released_year + is_short + pplcentr),
-  likes_elite = formula(video_likes ~ channel + log_n_sents + released_year + is_short + elite),
-  likes_pplcentr = formula(video_likes ~ channel + log_n_sents + released_year + is_short + pplcentr)
+  views_elite = formula(
+    video_views ~ channel + log_n_sents + released_year + is_short + elite
+  ),
+  views_pplcentr = formula(
+    video_views ~ channel + log_n_sents + released_year + is_short + pplcentr
+  ),
+  likes_elite = formula(
+    video_likes ~ channel + log_n_sents + released_year + is_short + elite
+  ),
+  likes_pplcentr = formula(
+    video_likes ~ channel + log_n_sents + released_year + is_short + pplcentr
+  )
 )
 
-quantiles <- c(0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99)
+quantiles <- c(
+  0.5,
+  0.6,
+  0.7,
+  0.75,
+  0.8,
+  0.85,
+  0.9,
+  0.91,
+  0.92,
+  0.93,
+  0.94,
+  0.95,
+  0.96,
+  0.97,
+  0.98,
+  0.99
+)
 
 for (model_type in names(model_types)) {
   print(paste("Starting model_type", model_type))
@@ -64,7 +89,9 @@ for (model_type in names(model_types)) {
 
     party_clean <- tolower(gsub("/", "_", party))
     fparty <- here(fbase, party_clean)
-    if (!dir.exists(fparty)) dir.create(fparty, recursive = T)
+    if (!dir.exists(fparty)) {
+      dir.create(fparty, recursive = T)
+    }
 
     for (quantile in quantiles) {
       fname <- paste0("q", quantile * 100, ".rds")
@@ -76,8 +103,12 @@ for (model_type in names(model_types)) {
       }
 
       print(paste(
-        "Starting quantile", quantile, ", party",
-        party, "model_type:", model_type
+        "Starting quantile",
+        quantile,
+        ", party",
+        party,
+        "model_type:",
+        model_type
       ))
 
       reg_df <- df |>
@@ -122,14 +153,21 @@ for (model_type in names(model_types)) {
         predictor_vars <- predictor_vars[predictor_vars != "channel"]
 
         # Create new formula without channel
-        new_formula_string <- paste(response_var, "~", paste(predictor_vars, collapse = " + "))
+        new_formula_string <- paste(
+          response_var,
+          "~",
+          paste(predictor_vars, collapse = " + ")
+        )
         current_formula <- as.formula(new_formula_string)
       } else {
         current_formula <- model_formula
       }
 
-
-      model <- bayes_model(form = current_formula, data = reg_df, quantile = quantile)
+      model <- bayes_model(
+        form = current_formula,
+        data = reg_df,
+        quantile = quantile
+      )
       saveRDS(model, fpath, compress = TRUE)
       model <- NULL
       gc()
